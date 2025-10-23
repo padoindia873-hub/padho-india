022e3a90b66e79b8419e0569a0f48e139f71d85b
padoindia873-hub <padoindia873@gmail.com>
2025-10-23 12:07:58 +0530

    first commit2

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://padhoindia.onrender.com/api",
+  baseURL: "https://padho-india-backend.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
-export const registerUser = (userData) => API.post("/user/register", userData);
+export const registerUser = (userData) => API.post("/auth/register", userData);
 
 export default API;
